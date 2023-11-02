85b2ce3f509edc217d9aa698395e3309112f6e5e
tamirsatti <tamer.satti@gmail.com>
2023-11-01 18:56:07 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for m in str:
+        if ord("a") <= ord(m) <= ord("z"):
+          m = chr(ord(m) - 32)
+        print("{:s}".format(m), end="")
+    print()
