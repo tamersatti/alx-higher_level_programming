73c60dbcb020200a3459d8d928728458858fcc38
tamirsatti <tamer.satti@gmail.com>
2023-11-05 01:08:44 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-      # Print sorted names from directories
+    # Print sorted names from directories
     for i in sorted(dir(hidden_4)):
         # print only name that donot start with __
         if i[:2] != '__':
